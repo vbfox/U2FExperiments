7307efcccb4acefe7a4d5bd5055217878c32d545
Julien Roncaglia <julien@roncaglia.fr>
2015-10-24 13:00:32 +0200

    Add missing Authentifier code

---
+++ TestApp/Tmp/Authentifier.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using BlackFox.U2F.Gnubby;
+using BlackFox.U2F.Key.messages;
+using Common.Logging;
+using JetBrains.Annotations;
+
+namespace U2FExperiments.Tmp
+{
+    extern alias LoggingPcl;
+
+    class Authentifier
+    {
+        static readonly TimeSpan timeBetweenRegisterCalls = TimeSpan.FromMilliseconds(200);
+        static readonly TimeSpan timeBetweenOpenCalls = TimeSpan.FromMilliseconds(200);
+        class KeyBusyException : Exception
+        {
+
+        }
+        static readonly ILog log = LoggingPcl::Common.Logging.LogManager.GetLogger(typeof(Signer));
+
+        readonly IKeyId keyId;
+        readonly ICollection<RegisterRequest> requests;
+
+        public Authentifier([NotNull] IKeyId keyId, [NotNull] ICollection<RegisterRequest> requests)
+        {
+            if (keyId == null)
+            {
+                throw new ArgumentNullException(nameof(keyId));
+            }
+            if (requests == null)
+            {
+                throw new ArgumentNullException(nameof(requests));
+            }
+
+            this.keyId = keyId;
+            this.requests = requests;
+        }
+
+        public async Task<AuthentifierResult> AuthenticateAsync(CancellationToken cancellationToken)
+        {
+            using (var key = await OpenKeyAsync(cancellationToken))
+            {
+                while (true)
+                {
+                    var signOnceResult = await TrySigningOnceAsync(key, cancellationToken);
+
+                    if (signOnceResult.HasValue)
+                    {
+                        return signOnceResult.Value;
+                    }
+
+                    await Task.Delay(timeBetweenRegisterCalls, cancellationToken);
+                }
+            }
+        }
+
+        async Task<AuthentifierResult?> TrySigningOnceAsync(IKey key, CancellationToken cancellationToken)
+        {
+            int challengesTried = 0;
+            foreach (var request in requests)
+            {
+                challengesTried += 1;
+
+                var requestSignResult = await TrySignOneRequest(key, request, cancellationToken);
+                if (requestSignResult.HasValue)
+                {
+                    return requestSignResult.Value;
+                }
+            }
+            if (challengesTried == 0)
+            {
+                return AuthentifierResult.Failure(KeyResponseStatus.Failure);
+            }
+
+            return null;
+        }
+
+        async Task<AuthentifierResult?> TrySignOneRequest(IKey key, RegisterRequest request,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await key.RegisterAsync(request, cancellationToken);
+
+                log.Info(result.Status.ToString());
+                switch (result.Status)
+                {
+                    case KeyResponseStatus.Success:
+                        return AuthentifierResult.Success(request, result.Data);
+                }
+            }
+            catch (KeyGoneException)
+            {
+                // No sense in continuing with this signer, the key isn't physically present anymore
+                log.DebugFormat("Key '{0}' is gone", keyId);
+                throw;
+            }
+            catch (TaskCanceledException)
+            {
+                // Let cancellation bubble up
+                throw;
+            }
+            catch (KeyBusyException)
+            {
+                // Maybe it won't be busy later
+            }
+            catch (Exception exception)
+            {
+                log.Error("Authenticate request failed", exception);
+            }
+            return null;
+        }
+
+        private async Task<IKey> OpenKeyAsync(CancellationToken cancellationToken)
+        {
+            IKey key;
+            do
+            {
+                try
+                {
+                    key = await keyId.OpenAsync(cancellationToken);
+                }
+                catch (KeyBusyException)
+                {
+                    key = null;
+                    await Task.Delay(timeBetweenOpenCalls, cancellationToken);
+                }
+            } while (key == null);
+            return key;
+        }
+    }
+}
---
+++ TestApp/Tmp/AuthentifierResult.cs
@@ -0,0 +1,30 @@
+﻿using BlackFox.U2F.Gnubby;
+using BlackFox.U2F.Key.messages;
+
+namespace U2FExperiments.Tmp
+{
+    internal struct AuthentifierResult
+    {
+        public bool IsSuccess => Status == KeyResponseStatus.Success;
+        public KeyResponseStatus Status { get; }
+        public RegisterRequest Request { get; }
+        public RegisterResponse Response { get; }
+
+        private AuthentifierResult(KeyResponseStatus status, RegisterRequest request, RegisterResponse response)
+        {
+            Status = status;
+            Request = request;
+            Response = response;
+        }
+
+        public static AuthentifierResult Success(RegisterRequest request, RegisterResponse response)
+        {
+            return new AuthentifierResult(KeyResponseStatus.Success, request, response);
+        }
+
+        public static AuthentifierResult Failure(KeyResponseStatus status)
+        {
+            return new AuthentifierResult(status, null, null);
+        }
+    }
+}
